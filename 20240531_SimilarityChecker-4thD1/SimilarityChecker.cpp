471e22c6234f8b5d76e768782cab6ee8ab235faa
beomsooo.kim <lightjd@naver.com>
2024-05-31 08:54:51 +0900

    [Green] SCLength60

---
+++ 20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+using std::pair;
+
+class SimilarityChecker {
+public:
+	int GetLengthScore(string n1, string n2) {
+		return 60;
+	}
+};
+
--- 20240531_SimilarityChecker-4thD1_gtest/test.cpp
+++ 20240531_SimilarityChecker-4thD1_gtest/test.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "../20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp"
 
-TEST(SimilarityCheckerSuite, test) {
-	EXPECT_EQ(1, 1);
+TEST(SimilarityCheckerSuite, SCLength60) {
+	SimilarityChecker sc;
+	int expected = 60;
+	EXPECT_EQ(expected, sc.GetLengthScore("ASD", "DSA"));
 }
