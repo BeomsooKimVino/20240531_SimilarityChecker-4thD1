09e072756c8acabdbf044d7814123dcdfc695a20
beomsooo.kim <lightjd@naver.com>
2024-05-31 10:58:36 +0900

    [Refactor] Function extract GetAlphaCnt

--- 20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp
+++ 20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <cmath>
 #include <unordered_set>
 
 using std::string;
-using std::pair;
 using std::unordered_set;
+using std::pair;
 
 struct Input {
 	string str1;
@@ -39,6 +38,15 @@ public:
 		unordered_set<char> usedAlpha1 = GetUsedUniqueAlphaSet(input.str1);
 		unordered_set<char> usedAlpha2 = GetUsedUniqueAlphaSet(input.str2);
 
+		pair<int, int> alphaCnt = GetAlphaCnt(usedAlpha1, usedAlpha2);
+		int totalAlphaCnt = alphaCnt.first;
+		int samelAlphaCnt = alphaCnt.second;
+
+		return MAX_APPERENCE_SCORE * (static_cast<double>(samelAlphaCnt) / totalAlphaCnt);
+	}
+
+	pair<int, int> GetAlphaCnt(std::unordered_set<char>& usedAlpha2, std::unordered_set<char>& usedAlpha1)
+	{
 		int totalAlphaCnt = usedAlpha1.size();
 		int samelAlphaCnt = 0;
 		for (char used : usedAlpha2)
@@ -46,8 +54,7 @@ public:
 			if (IsNotApperence(usedAlpha1, used)) totalAlphaCnt++;
 			else samelAlphaCnt++;
 		}
-
-		return MAX_APPERENCE_SCORE * (static_cast<double>(samelAlphaCnt) / totalAlphaCnt);
+		return { totalAlphaCnt, samelAlphaCnt };
 	}
 
 private:
