b7a19553912c252917670d7e5f046c3d72284d20
beomsooo.kim <lightjd@naver.com>
2024-05-31 10:51:41 +0900

    [Refactor] Applied Fixture

--- 20240531_SimilarityChecker-4thD1_gtest/test.cpp
+++ 20240531_SimilarityChecker-4thD1_gtest/test.cpp
@@ -1,58 +1,48 @@
 #include "pch.h"
 #include "../20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp"
 
-TEST(SimilarityCheckerSuite, SCLength60) {
+class SimilarityCheckerFixture : public testing::Test {
+public:
 	SimilarityChecker sc;
-	Input input = { "ASD", "DSA" };
-	int expected = 60;
-	EXPECT_EQ(expected, sc.GetLengthScore(input));
+	void CheckLengthScore(Input input, int expected)
+	{
+		EXPECT_EQ(expected, sc.GetLengthScore(input));
+	}
+	void CheckApperenceScore(Input input, int expected)
+	{
+		EXPECT_EQ(expected, sc.GetApperenceScore(input));
+	}
+};
+
+TEST_F(SimilarityCheckerFixture, SCLength60) {
+	CheckLengthScore({ "ASD", "DSA" }, 60);
 }
 
-TEST(SimilarityCheckerSuite, SCLength0) {
-	SimilarityChecker sc;
-	Input input = { "A", "BB" };
-	int expected = 0;
-	EXPECT_EQ(expected, sc.GetLengthScore(input));
+TEST_F(SimilarityCheckerFixture, SCLength0) {
+	CheckLengthScore({ "A", "BB" }, 0);
 }
 
-TEST(SimilarityCheckerSuite, SCLengthGapScore20) {
-	SimilarityChecker sc;
-	Input input = { "AAABB", "BAA" };
-	int expected = 20;
-	EXPECT_EQ(expected, sc.GetLengthScore(input));
+TEST_F(SimilarityCheckerFixture, SCLengthGapScore20) {
+	CheckLengthScore({ "AAABB", "BAA" }, 20);
 }
 
-TEST(SimilarityCheckerSuite, SCLengthGapScore30) {
-	SimilarityChecker sc;
-	Input input = { "AA", "AAE" };
-	int expected = 30;
-	EXPECT_EQ(expected, sc.GetLengthScore(input));
+TEST_F(SimilarityCheckerFixture, SCLengthGapScore30) {
+	CheckLengthScore({ "AA", "AAE" }, 30);
 }
 
-TEST(SimilarityCheckerSuite, SCApperenceScore40) {
-	SimilarityChecker sc;
-	Input input = { "ASD", "DSA" };
-	int expected = 40;
-	EXPECT_EQ(expected, sc.GetApperenceScore(input));
+TEST_F(SimilarityCheckerFixture, SCApperenceScore40) {
+	CheckApperenceScore({ "ASD", "DSA" }, 40);
 }
 
-TEST(SimilarityCheckerSuite, SCApperenceScore0) {
-	SimilarityChecker sc;
-	Input input = { "A", "BB" };
-	int expected = 0;
-	EXPECT_EQ(expected, sc.GetApperenceScore(input));
+TEST_F(SimilarityCheckerFixture, SCApperenceScore0) {
+
+	CheckApperenceScore({ "A", "BB" }, 0);
 }
 
-TEST(SimilarityCheckerSuite, SCApperenceScore40SameAlpha) {
-	SimilarityChecker sc;
-	Input input = { "AAABB", "BA" };
-	int expected = 40;
-	EXPECT_EQ(expected, sc.GetApperenceScore(input));
+TEST_F(SimilarityCheckerFixture, SCApperenceScore40SameAlpha) {
+	CheckApperenceScore({ "AAABB", "BA" }, 40);
 }
 
-TEST(SimilarityCheckerSuite, SCApperenceScorer20PartialScore) {
-	SimilarityChecker sc;
-	Input input = { "AA", "AAB" };
-	int expected = 20;
-	EXPECT_EQ(expected, sc.GetApperenceScore(input));
+TEST_F(SimilarityCheckerFixture, SCApperenceScorer20PartialScore) {
+	CheckApperenceScore({ "AA", "AAB" }, 20);
 }
