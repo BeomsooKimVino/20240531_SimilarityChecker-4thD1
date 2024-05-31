801a68c692155bd9be05884628a3cfbf270db7f5
beomsooo.kim <lightjd@naver.com>
2024-05-31 09:41:15 +0900

    [Refactor] GetApperenceScore0

--- 20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp
+++ 20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp
@@ -37,17 +37,18 @@ public:
 		std::sort(input.str1.begin(), input.str1.end());
 		std::sort(input.str2.begin(), input.str2.end());
 		if (input.str1 == input.str2)
-			return 40;
+			return MAX_APPERENCE_SCORE;
 		return 0;
 	}
 
 private:
-	const int MAX_GAPSCORE = 60;
+	const int MAX_GAP_SCORE = 60;
+	const int MAX_APPERENCE_SCORE = 40;
 	int GetGapScore(int strLength1, int strLength2)
 	{
 		int gap = std::abs(strLength1 - strLength2);
 		int minLength = std::min(strLength1, strLength2);
 		double gapscoreRational = 1 - (static_cast<double>(gap) / minLength);
-		return static_cast<int>(MAX_GAPSCORE * gapscoreRational);
+		return static_cast<int>(MAX_GAP_SCORE * gapscoreRational);
 	}
 };
