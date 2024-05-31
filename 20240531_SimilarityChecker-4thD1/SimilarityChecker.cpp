cb1d90f67512f4192a291b42ad99f0b9734f2053
beomsooo.kim <lightjd@naver.com>
2024-05-31 09:27:58 +0900

    [Refactor] SCLengthGapScore30 with Fixed wrong implementation for GetLengthScore

--- 20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp
+++ 20240531_SimilarityChecker-4thD1/SimilarityChecker.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cmath>
 
 using std::string;
 using std::pair;
@@ -13,29 +14,32 @@ struct Input {
 class SimilarityChecker {
 public:
 	int GetLengthScore(Input input) {
-		if (input.str1 == "AA" && input.str2 == "AAE")
-			return 30;
+		int strLength1 = input.str1.length();
+		int strLength2 = input.str2.length();
 
-		std::sort(input.str1.begin(), input.str1.end());
-		std::sort(input.str2.begin(), input.str2.end());
-
-		if (input.str1 == input.str2)
+		if (strLength1 == strLength2)
 		{
 			return 60;
 		}
-		else if (input.str1.length() >= input.str2.length() * 2
-			|| input.str2.length() >= input.str1.length() * 2)
+		else if (strLength1 >= strLength2 * 2
+			|| strLength2 >= strLength1 * 2)
 		{
 			return 0;
 		}
 		else
 		{
-			int gap = input.str1.length() - input.str2.length();
-			if (gap < 0)
-				gap *= -1;
-			int minLength = std::min(input.str1.length(), input.str2.length());
-			return static_cast<int>(60 * (1 - static_cast<double>(gap) / minLength));
+			return GetGapScore(strLength1, strLength2);
 		}
+	}
+
+private:
+	const int MAX_GAPSCORE = 60;
+	int GetGapScore(int strLength1, int strLength2)
+	{
+		int gap = std::abs(strLength1 - strLength2);
+		int minLength = std::min(strLength1, strLength2);
+		double gapscoreRational = 1 - (static_cast<double>(gap) / minLength);
+		return static_cast<int>(MAX_GAPSCORE * gapscoreRational);
 	}
 };
 
